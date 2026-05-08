e0b0d0d2f7f1f1fb53cf792d5f236a4fd3241725
Gaurav2794 <gaurav274chavan@gmail.com>
2026-05-08 16:58:24 +0530

    assignment15

---
+++ DSA_ASSIGNMENTS/03-DoublyLinkedList/Assignment15/assignment15.c
@@ -0,0 +1,104 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct node {
+    int data;
+    struct node *prev;
+    struct node *next;
+} Node;
+
+Node* createNode(int value) {
+    Node* newNode = (Node*)malloc(sizeof(Node));
+
+    if (newNode == NULL) {
+        printf("Memory allocation failed!\n");
+        exit(1);
+    }
+
+    newNode->data = value;
+    newNode->prev = NULL;
+    newNode->next = NULL;
+
+    return newNode;
+}
+
+Node* insertEnd(Node* head, int value) {
+    Node* newNode = createNode(value);
+
+    if (head == NULL)
+        return newNode;
+
+    Node* temp = head;
+
+    while (temp->next != NULL) {
+        temp = temp->next;
+    }
+
+    temp->next = newNode;
+    newNode->prev = temp;
+
+    return head;
+}
+
+void display(Node* head) {
+    Node* temp = head;
+
+    while (temp != NULL) {
+        printf("%d <-> ", temp->data);
+        temp = temp->next;
+    }
+
+    printf("NULL\n");
+}
+
+void findMiddle(Node* head) {
+    if (head == NULL) {
+        printf("List is empty!\n");
+        return;
+    }
+
+    Node* slow = head;
+    Node* fast = head;
+
+    while (fast != NULL && fast->next != NULL) {
+        slow = slow->next;
+        fast = fast->next->next;
+    }
+
+    printf("Middle node is: %d\n", slow->data);
+}
+
+Node* destroyList(Node* head) {
+    Node* temp;
+
+    while (head != NULL) {
+        temp = head;
+        head = head->next;
+        free(temp);
+    }
+
+    return NULL;
+}
+
+int main() {
+    Node* head = NULL;
+    int n, value, i;
+
+    printf("Enter number of nodes: ");
+    scanf("%d", &n);
+
+    for (i = 1; i <= n; i++) {
+        printf("Enter value for node %d: ", i);
+        scanf("%d", &value);
+        head = insertEnd(head, value);
+    }
+
+    printf("\nDoubly Linked List:\n");
+    display(head);
+
+    findMiddle(head);
+
+    head = destroyList(head);
+
+    return 0;
+}
