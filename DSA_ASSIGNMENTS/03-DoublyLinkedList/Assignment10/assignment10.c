9d77b1340d91f537a08c9d4227bb134042130735
Gaurav2794 <gaurav274chavan@gmail.com>
2026-05-08 16:35:28 +0530

    assignment10

---
+++ DSA_ASSIGNMENTS/03-DoublyLinkedList/Assignment10/assignment10.c
@@ -0,0 +1,130 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct node {
+    int data;
+    struct node* prev;
+    struct node* next;
+} Node;
+
+Node* createNode(int value) {
+    Node* newNode = (Node*)malloc(sizeof(Node));
+
+    if (newNode == NULL) {
+        printf("Memory allocation failed!\n");
+        exit(1);
+    }
+
+    newNode->data = value;
+    newNode->prev = NULL;
+    newNode->next = NULL;
+
+    return newNode;
+}
+
+Node* insertEnd(Node* head, int value) {
+    Node* newNode = createNode(value);
+
+    if (head == NULL)
+        return newNode;
+
+    Node* temp = head;
+
+    while (temp->next != NULL) {
+        temp = temp->next;
+    }
+
+    temp->next = newNode;
+    newNode->prev = temp;
+
+    return head;
+}
+
+Node* deleteByValue(Node* head, int key) {
+    if (head == NULL) {
+        printf("List is empty!\n");
+        return NULL;
+    }
+
+    Node* temp = head;
+
+    while (temp != NULL && temp->data != key) {
+        temp = temp->next;
+    }
+
+    if (temp == NULL) {
+        printf("Value not found!\n");
+        return head;
+    }
+
+    if (temp == head) {
+        head = head->next;
+
+        if (head != NULL)
+            head->prev = NULL;
+
+        free(temp);
+        return head;
+    }
+
+    if (temp->next != NULL)
+        temp->next->prev = temp->prev;
+
+    temp->prev->next = temp->next;
+
+    free(temp);
+
+    return head;
+}
+
+void display(Node* head) {
+    Node* temp = head;
+
+    while (temp != NULL) {
+        printf("%d <-> ", temp->data);
+        temp = temp->next;
+    }
+
+    printf("NULL\n");
+}
+
+Node* destroyList(Node* head) {
+    Node* temp;
+
+    while (head != NULL) {
+        temp = head;
+        head = head->next;
+        free(temp);
+    }
+
+    return NULL;
+}
+
+int main() {
+    Node* head = NULL;
+    int n, value, key, i;
+
+    printf("Enter number of nodes: ");
+    scanf("%d", &n);
+
+    for (i = 1; i <= n; i++) {
+        printf("Enter value for node %d: ", i);
+        scanf("%d", &value);
+        head = insertEnd(head, value);
+    }
+
+    printf("\nOriginal List:\n");
+    display(head);
+
+    printf("\nEnter value to delete: ");
+    scanf("%d", &key);
+
+    head = deleteByValue(head, key);
+
+    printf("\nUpdated List:\n");
+    display(head);
+
+    head = destroyList(head);
+
+    return 0;
+}
