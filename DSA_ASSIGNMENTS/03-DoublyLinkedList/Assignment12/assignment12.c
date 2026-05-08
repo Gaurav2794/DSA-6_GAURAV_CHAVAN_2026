3b8fde37e20442389f0d68ce15a4a3cf907fff77
Gaurav2794 <gaurav274chavan@gmail.com>
2026-05-08 16:52:11 +0530

    assignment12

---
+++ DSA_ASSIGNMENTS/03-DoublyLinkedList/Assignment12/assignment12.c
@@ -0,0 +1,114 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct node {
+    int data;
+    struct node *prev;
+    struct node *next;
+} Node;
+
+Node* createNode(int value) {
+    Node* newNode = (Node*)malloc(sizeof(Node));
+
+    if (newNode == NULL) {
+        printf("Memory allocation failed!\n");
+        exit(1);
+    }
+
+    newNode->data = value;
+    newNode->prev = NULL;
+    newNode->next = NULL;
+
+    return newNode;
+}
+
+Node* insertEnd(Node* head, int value) {
+    Node* newNode = createNode(value);
+
+    if (head == NULL)
+        return newNode;
+
+    Node* temp = head;
+
+    while (temp->next != NULL)
+        temp = temp->next;
+
+    temp->next = newNode;
+    newNode->prev = temp;
+
+    return head;
+}
+
+Node* sortList(Node* head) {
+    if (head == NULL)
+        return NULL;
+
+    Node *current, *index;
+    int temp;
+
+    for (current = head; current != NULL; current = current->next) {
+        for (index = current->next; index != NULL; index = index->next) {
+            if (current->data > index->data) {
+                temp = current->data;
+                current->data = index->data;
+                index->data = temp;
+            }
+        }
+    }
+
+    return head;
+}
+
+void display(Node* head) {
+    Node* temp = head;
+
+    if (head == NULL) {
+        printf("List is empty!\n");
+        return;
+    }
+
+    while (temp != NULL) {
+        printf("%d <-> ", temp->data);
+        temp = temp->next;
+    }
+
+    printf("NULL\n");
+}
+
+Node* destroyList(Node* head) {
+    Node* temp;
+
+    while (head != NULL) {
+        temp = head;
+        head = head->next;
+        free(temp);
+    }
+
+    return NULL;
+}
+
+int main() {
+    Node* head = NULL;
+    int n, value, i;
+
+    printf("Enter number of nodes: ");
+    scanf("%d", &n);
+
+    for (i = 1; i <= n; i++) {
+        printf("Enter value for node %d: ", i);
+        scanf("%d", &value);
+        head = insertEnd(head, value);
+    }
+
+    printf("\nOriginal List:\n");
+    display(head);
+
+    head = sortList(head);
+
+    printf("\nSorted List:\n");
+    display(head);
+
+    head = destroyList(head);
+
+    return 0;
+}
