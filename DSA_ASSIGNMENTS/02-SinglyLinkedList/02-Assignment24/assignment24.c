4cc22e4c54a7e3104afa8b69f2173f4e444e8c6c
Gaurav2794 <gaurav274chavan@gmail.com>
2026-05-08 17:24:42 +0530

    assignment24

---
+++ DSA_ASSIGNMENTS/02-SinglyLinkedList/02-Assignment24/assignment24.c
@@ -0,0 +1,122 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+typedef struct node {
+    int id;
+    char name[20];
+    struct node *next;
+} Node;
+
+Node* createNode(int id, char name[]) {
+    Node* newNode = (Node*)malloc(sizeof(Node));
+
+    if (newNode == NULL) {
+        printf("Memory allocation failed!\n");
+        exit(1);
+    }
+
+    newNode->id = id;
+    strcpy(newNode->name, name);
+    newNode->next = NULL;
+
+    return newNode;
+}
+
+Node* insertEnd(Node* head, int id, char name[]) {
+    Node* newNode = createNode(id, name);
+
+    if (head == NULL)
+        return newNode;
+
+    Node* temp = head;
+
+    while (temp->next != NULL)
+        temp = temp->next;
+
+    temp->next = newNode;
+
+    return head;
+}
+
+Node* deleteBeforeKey(Node* head, int key) {
+    if (head == NULL || head->next == NULL) {
+        printf("Not enough nodes!\n");
+        return head;
+    }
+
+    if (head->next->id == key) {
+        Node* temp = head;
+        head = head->next;
+        free(temp);
+        return head;
+    }
+
+    Node* prevPrev = NULL;
+    Node* prev = head;
+    Node* current = head->next;
+
+    while (current != NULL && current->id != key) {
+        prevPrev = prev;
+        prev = current;
+        current = current->next;
+    }
+
+    if (current == NULL) {
+        printf("Key not found!\n");
+        return head;
+    }
+
+    prevPrev->next = current;
+    free(prev);
+
+    return head;
+}
+
+void display(Node* head) {
+    Node* temp = head;
+
+    while (temp != NULL) {
+        printf("|_%d_|_%s_| --> ", temp->id, temp->name);
+        temp = temp->next;
+    }
+
+    printf("NULL\n");
+}
+
+Node* destroyList(Node* head) {
+    Node* temp;
+
+    while (head != NULL) {
+        temp = head;
+        head = head->next;
+        free(temp);
+    }
+
+    return NULL;
+}
+
+int main() {
+    Node* head = NULL;
+    int key;
+
+    head = insertEnd(head, 1, "AA");
+    head = insertEnd(head, 2, "BB");
+    head = insertEnd(head, 3, "CC");
+    head = insertEnd(head, 4, "DD");
+
+    printf("Original List:\n");
+    display(head);
+
+    printf("\nEnter key: ");
+    scanf("%d", &key);
+
+    head = deleteBeforeKey(head, key);
+
+    printf("\nUpdated List:\n");
+    display(head);
+
+    head = destroyList(head);
+
+    return 0;
+}
