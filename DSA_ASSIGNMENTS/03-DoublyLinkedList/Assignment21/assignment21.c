bc2aa5bd7103f255fa1a7a07d8a09921a20c5be9
Gaurav2794 <gaurav274chavan@gmail.com>
2026-05-08 17:13:10 +0530

    assignment21

---
+++ DSA_ASSIGNMENTS/03-DoublyLinkedList/Assignment21/assignment21.c
@@ -0,0 +1,120 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+typedef struct node {
+    int id;
+    char name[20];
+    struct node *next;
+} Node;
+
+Node* createNode(int id, char name[]) {
+    Node* newNode = (Node*)malloc(sizeof(Node));
+
+    if (newNode == NULL) {
+        printf("Memory allocation failed!\n");
+        exit(1);
+    }
+
+    newNode->id = id;
+    strcpy(newNode->name, name);
+    newNode->next = NULL;
+
+    return newNode;
+}
+
+Node* insertEnd(Node* head, int id, char name[]) {
+    Node* newNode = createNode(id, name);
+
+    if (head == NULL)
+        return newNode;
+
+    Node* temp = head;
+
+    while (temp->next != NULL)
+        temp = temp->next;
+
+    temp->next = newNode;
+
+    return head;
+}
+
+Node* insertAfterKey(Node* head, int key, int newId, char newName[]) {
+    if (head == NULL) {
+        printf("List is empty!\n");
+        return head;
+    }
+
+    Node* temp = head;
+
+    while (temp != NULL && temp->id != key) {
+        temp = temp->next;
+    }
+
+    if (temp == NULL) {
+        printf("Key not found!\n");
+        return head;
+    }
+
+    Node* newNode = createNode(newId, newName);
+
+    newNode->next = temp->next;
+    temp->next = newNode;
+
+    return head;
+}
+
+void display(Node* head) {
+    Node* temp = head;
+
+    while (temp != NULL) {
+        printf("|_%d_|_%s_| --> ", temp->id, temp->name);
+        temp = temp->next;
+    }
+
+    printf("NULL\n");
+}
+
+Node* destroyList(Node* head) {
+    Node* temp;
+
+    while (head != NULL) {
+        temp = head;
+        head = head->next;
+        free(temp);
+    }
+
+    return NULL;
+}
+
+int main() {
+    Node* head = NULL;
+    int key, newId;
+    char newName[20];
+
+    head = insertEnd(head, 1, "AA");
+    head = insertEnd(head, 2, "BB");
+    head = insertEnd(head, 3, "CC");
+    head = insertEnd(head, 4, "DD");
+
+    printf("Original List:\n");
+    display(head);
+
+    printf("\nEnter key: ");
+    scanf("%d", &key);
+
+    printf("Enter new ID: ");
+    scanf("%d", &newId);
+
+    printf("Enter new Name: ");
+    scanf("%s", newName);
+
+    head = insertAfterKey(head, key, newId, newName);
+
+    printf("\nUpdated List:\n");
+    display(head);
+
+    head = destroyList(head);
+
+    return 0;
+}
